8ecf1597c8c6b49a9418afdd567661697a5d4177
Iñigo Montánchez <mci.m89@gmail.com>
2017-07-29 15:29:36 +0200

    Se crea base de datos en caso de no existir en la conexion

--- php/conexion.php
+++ php/conexion.php
@@ -1,19 +1,42 @@
-<?php 
-	// Connect to MySQL
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
+// Create database
+$sql = "CREATE DATABASE PEPE";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
 
-
-
-	try{
-		$link=new PDO("CREATE DATABASE IF NOT EXISTS pokemon_php");
-		$link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-		$base= new PDO('mysql:host=localhost; dbname=pokemon_php', 'root', 'root');
-		$base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$base->exec("SET CHARACTER SET UTF8");
-	}catch(Exception $e){
-		die('Error : ' . $e->getMessage());
-		echo "Linea del error = " . $e->getLine();
-	}
+$conn->close();
 ?>
+
+
+
+
+<?php 
+	// // Connect to MySQL
+
+
+	// try{
+	
+
+	// 	$base= new PDO('mysql:host=localhost; dbname=pokemon_php', 'root', 'root');
+	// 	$base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// 	$base->exec("SET CHARACTER SET UTF8");
+	// }catch(Exception $e){
+	// 	die('Error : ' . $e->getMessage());
+	// 	echo "Linea del error = " . $e->getLine();
+	// }
+?>
+
