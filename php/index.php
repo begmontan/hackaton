d1f09b33c464017acfbc264afcb2c40bbeb4a986
Iñigo Montánchez <mci.m89@gmail.com>
2017-07-29 14:12:31 +0200

    Modifica la ruta css del index.php

--- php/index.php
+++ php/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>CRUD</title>
-  <link rel="stylesheet" type="text/css" href="hoja.css">
+  <link rel="stylesheet" type="text/css" href="../css/hoja.css">
 
   <!-- Latest compiled and minified CSS -->
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
