ea288b6842210cacb33b69c17dad2f0d992ba66e
Begoña Montanchez <begmontan@gmail.com>
2017-07-29 14:37:04 +0200

    Incluye creacion de base de datos

--- php/conexion.php
+++ php/conexion.php
@@ -1,14 +1,32 @@
 <?php 
+	// Connect to MySQL
+	$link = mysql_connect('localhost', 'root', 'root');
+	if (!$link) {
+	    die('Could not connect: ' . mysql_error());
+	}
 
-try{
+	// Make my_db the current database
+	$db_selected = mysql_select_db('pokemon_php', $link);
 
-$base= new PDO('mysql:host=localhost; dbname=pokemon_php', 'root', 'root');
-$base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$base->exec("SET CHARACTER SET UTF8");
+	if (!$db_selected) {
+	  // If we couldn't, then it either doesn't exist, or we can't see it.
+	  $sql = 'CREATE DATABASE pokemon_php';
 
-}catch(Exception $e){
-	die('Error : ' . $e->getMessage());
-	echo "Linea del error = " . $e->getLine();
-}
+	  if (mysql_query($sql, $link)) {
+	      echo "Database my_db created successfully\n";
+	  } else {
+	      echo 'Error creating database: ' . mysql_error() . "\n";
+	  }
+	}
 
- ?>
+	mysql_close($link);
+
+	try{
+		$base= new PDO('mysql:host=localhost; dbname=pokemon_php', 'root', 'root');
+		$base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$base->exec("SET CHARACTER SET UTF8");
+	}catch(Exception $e){
+		die('Error : ' . $e->getMessage());
+		echo "Linea del error = " . $e->getLine();
+	}
+?>
