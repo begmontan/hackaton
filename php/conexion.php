9b1fe60826d198290a48d17d936bedd3f79bbc79
Iñigo Montánchez <mci.m89@gmail.com>
2017-07-29 14:56:19 +0200

    prueba de conexion modificacada

--- php/conexion.php
+++ php/conexion.php
@@ -1,27 +1,14 @@
 <?php 
 	// Connect to MySQL
-	$link = mysql_connect('localhost', 'root', 'root');
-	if (!$link) {
-	    die('Could not connect: ' . mysql_error());
-	}
 
-	// Make my_db the current database
-	$db_selected = mysql_select_db('pokemon_php', $link);
 
-	if (!$db_selected) {
-	  // If we couldn't, then it either doesn't exist, or we can't see it.
-	  $sql = 'CREATE DATABASE pokemon_php';
 
-	  if (mysql_query($sql, $link)) {
-	      echo "Database my_db created successfully\n";
-	  } else {
-	      echo 'Error creating database: ' . mysql_error() . "\n";
-	  }
-	}
 
-	mysql_close($link);
 
 	try{
+		$link=new PDO("CREATE DATABASE IF NOT EXISTS pokemon_php");
+		$link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 		$base= new PDO('mysql:host=localhost; dbname=pokemon_php', 'root', 'root');
 		$base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$base->exec("SET CHARACTER SET UTF8");
